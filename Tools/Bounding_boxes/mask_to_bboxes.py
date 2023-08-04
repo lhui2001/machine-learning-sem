e41b565cbdc8d3b2743588c45b011ae8c8994b5c
Lucy Hui <lucyhui2000@gmail.com>
2023-08-04 09:44:40 -0400

    Update mask_to_bboxes.py

--- Tools/Bounding_boxes/mask_to_bboxes.py
+++ Tools/Bounding_boxes/mask_to_bboxes.py
@@ -1,7 +1,8 @@
 # https://github.com/nikhilroxtomar/Semantic-Segmentation-Mask-to-Bounding-Box
 # https://www.youtube.com/watch?v=RmLDL7AVXUc&t=684s&pp=ygUUbWFzayB0byBib3VuZGluZyBib3g%3D
-# Unedited code from source
+# only works with png 
 
+import pandas as pd
 import os
 import numpy as np
 import cv2
@@ -9,26 +10,6 @@ from glob import glob
 from tqdm import tqdm
 from skimage.measure import label, regionprops, find_contours
 
-""" Creating a directory """
-def create_dir(path):
-    if not os.path.exists(path):
-        os.makedirs(path)
-
-""" Convert a mask to border image """
-def mask_to_border(mask):
-    h, w = mask.shape
-    border = np.zeros((h, w))
-
-    contours = find_contours(mask, 128)
-    for contour in contours:
-        for c in contour:
-            x = int(c[0])
-            y = int(c[1])
-            border[x][y] = 255
-
-    return border
-
-""" Convert a mask to bounding boxes """
 def mask_to_bbox(mask):
     bboxes = []
 
@@ -46,35 +27,23 @@ def mask_to_bbox(mask):
 
     return bboxes
 
-def parse_mask(mask):
-    mask = np.expand_dims(mask, axis=-1)
-    mask = np.concatenate([mask, mask, mask], axis=-1)
-    return mask
-
 if __name__ == "__main__":
     """ Load the dataset """
-    images = sorted(glob(os.path.join("data", "image", "*")))  # load images frome directory data containing directory image in current working directory
-    masks = sorted(glob(os.path.join("data", "mask", "*")))    # load masks from directory data containing directory mask in current working directory
-
-    """ Create folder to save images """
-    create_dir("results")
-
-    """ Loop over the dataset """
+    images = sorted(glob(os.path.join("data", "image", "*")))
+    masks = sorted(glob(os.path.join("data", "mask", "*")))
+    
     for x, y in tqdm(zip(images, masks), total=len(images)):
+        
         """ Extract the name """
-        name = x.split("/")[-1].split(".")[0]
-
+        name = os.path.splitext(os.path.basename(x))[0]
+        
         """ Read image and mask """
         x = cv2.imread(x, cv2.IMREAD_COLOR)
         y = cv2.imread(y, cv2.IMREAD_GRAYSCALE)
-
+        
         """ Detecting bounding boxes """
         bboxes = mask_to_bbox(y)
-
-        """ marking bounding box on image """
-        for bbox in bboxes:
-            x = cv2.rectangle(x, (bbox[0], bbox[1]), (bbox[2], bbox[3]), (255, 0, 0), 2)
-
-        """ Saving the image """
-        cat_image = np.concatenate([x, parse_mask(y)], axis=1)
-        cv2.imwrite(f"results/{name}.png", cat_image)
+        
+        """ Save to txt """
+        df = pd.DataFrame(data=bboxes)
+        np.savetxt(f"C:/.../{name}.txt", df.values, fmt='%d')
